6d84aac87ed517160a2d7d01506bd846d6ac8d7c
Omwano <abekahellena@gmail.com>
2023-10-07 13:18:27 +0300

    add Hero class

---
+++ src/main/java/com/niawetu/Hero.java
@@ -0,0 +1,16 @@
+package com.niawetu;
+
+public class Hero {
+    private String name;
+    private int age;
+    private String power;
+    private String weakness;
+
+    public Hero(String name, int age, String power, String weakness) {
+        this.name = name;
+        this.age = age;
+        this.power = power;
+        this.weakness = weakness;
+    }
+
+}
